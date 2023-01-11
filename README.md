4bf29ab8a2e2a4f8cda8d852adb41382b7b549d8
Lawrence Lim <you@example.com>
2023-01-11 16:05:05 +0800

    Added install script, readme, and .local/bin

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles Repo
+
+## Installation
+1. Clone the repository
+    ```shell
+    git clone --recurse-submodules <github-repo-url>
+    ```
+2. Run the install script
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# Clone git repo and recurse submodules
+  # .config folder and $HOME directory
+# Conditionally install homebrew
+# Install zsh, curl, tar, unzip, and all the essential stuff
+# Install Homebrew and sudo apt install build-essential
+# If Homebrew is able to be installed, install important deps
+# Create symlinks for all the files at the correct point in times
