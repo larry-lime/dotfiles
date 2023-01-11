ee92c23a36b8e9b534fcfbdc64a18b718e38f3c4
Lawrence Lim <you@example.com>
2023-01-12 04:05:15 +0800

    updated stuff

--- README.md
+++ README.md
@@ -4,9 +4,16 @@
 1. Install Libraries
 ```shell
 sudo apt update
-sudo apt install xinit xdg-utils awesome kitty xterm firefox xdg-utils
+sudo apt install curl git htop
+sudo apt install gnome-tweaks gnome-shell-extensions
 ```
 2. Install Configurations
     ```shell
-    git clone --recurse-submodules https://github.com/larry-lime/dotfiles ~/.dotfiles && ~/.dotfiles/install-ubuntu-arm.sh
+    git clone --recurse-submodules https://github.com/larry-lime/dotfiles
+    cd ~/.dotfiles
+    ./install.sh
     ```
+## Additional Apps
+```shell
+sudo snap install lazygit
+```
--- install.sh
+++ install.sh
@@ -1,27 +1,14 @@
-# Install Essential Tools
+# Install Zsh
 sudo apt update
-sudo dmenu zsh fzf bat gh 
+sudo apt install zsh cargo
+
 # Change Default Shell to Zsh
 chsh -s $(which zsh)
 
+brew install fzf bat gh node ripgrep exa fd neovim tmux
+
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Install Node
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
-
-# Installing Useful CLI Tools
-cargo install fd-find ripgrep exa
-
-# Building Neovim
-sudo apt-get install build-essential ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-
-# Check if Builds directory exists. If it doesn't make it and install neovim there
-mkdir ~/Builds
-git clone https://github.com/neovim/neovim ~/Builds/neovim
-cd ~/Builds/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
 
 # Source Z-Shell
 source ~/.config/zsh/.zshrc
@@ -33,7 +20,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.dotfiles/tmux/.tmux/plugins/tpm
 # Creating Symlinks for files
 ln -s ~/.dotfiles/.fzf.zsh ~/.fzf.zsh
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-rm ~/.zshenv && ln -s ~/.dotfiles/.zshenv ~/.zshenv
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
 
 # Creating Symlinks for directories
 ln -s ~/.dotfiles/.local/bin ~/.local/bin
@@ -41,6 +28,4 @@ ln -s ~/.dotfiles/zsh ~/.config/zsh
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.dotfiles/awesome ~/.config/awesome
-rmdir ~/.config/kitty && ln -s ~/.dotfiles/kitty ~/.config/kitty
-
-cd ~ && mkdir Projects Documents Downloads Github-Repos Work
+ln -s ~/.dotfiles/kitty ~/.config/kitty
