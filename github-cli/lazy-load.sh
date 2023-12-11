bb89c68a3ff6cc5b57fe7419bc4f625e07e4313f
Lawrence Lim <ll4715@nyu.edu>
2023-12-11 15:58:33 -0500

    updates

--- github-cli/lazy-load.sh
+++ github-cli/lazy-load.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
+
+# check if github copilot is installed
+if ! command -v github-copilot-cli &> /dev/null
+then
+    echo "github-copilot-cli could not be found"
+    exit 1
+fi
+
 if [[ $GCLI == false ]]; then
-  eval "$(github-copilot-cli alias -- "$0")"
-fi 
+    eval "$(github-copilot-cli alias -- "$0")"
+fi
 GCLI=true
